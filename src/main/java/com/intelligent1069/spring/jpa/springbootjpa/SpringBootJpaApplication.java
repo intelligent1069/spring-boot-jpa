c38b96676ac243607053b3462a664b621585efaf
Virus <Virus@LAPTOP-B3JULT99>
2019-02-16 17:01:46 +0800

    Initial Check In

---
+++ src/main/java/com/intelligent1069/spring/jpa/springbootjpa/SpringBootJpaApplication.java
@@ -0,0 +1,14 @@
+package com.intelligent1069.spring.jpa.springbootjpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootJpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootJpaApplication.class, args);
+	}
+
+}
+
